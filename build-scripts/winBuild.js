cb66a1cb84ab38fedc8039b35c67ca5d9f2541bb
Bhanuka <bhanuka.yd@gmail.com>
2019-03-15 16:14:44 +0530

    added electron win builder

--- build-scripts/winBuild.js
+++ build-scripts/winBuild.js
@@ -1,9 +1,12 @@
 var electronInstaller = require('electron-winstaller');
 var path = require("path");
+var fs = require("fs");
 
 const appDir = path.join(__dirname, "..", "dist", "win-unpacked");
 const outDir = path.join(__dirname, "..", "win_build");
 const gifPath = path.join("build-scripts","load.gif");
+
+fs.writeFileSync(path.join(appDir,"LICENSE"),"TEST LICENSE");
 
 resultPromise = electronInstaller.createWindowsInstaller({
     appDirectory: appDir.toString(),
