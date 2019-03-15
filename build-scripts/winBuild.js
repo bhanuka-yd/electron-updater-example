110387f892480dbaf90f68afa76ecfba4b901dfe
Bhanuka <bhanuka.yd@gmail.com>
2019-03-15 13:45:13 +0530

    added electron win builder

--- build-scripts/winBuild.js
+++ build-scripts/winBuild.js
@@ -1,16 +1,16 @@
 var electronInstaller = require('electron-winstaller');
 var path = require("path");
 
-const appDir = (path.join(__dirname,"..","dist","win-unpacked"));
-const outDir = (path.join(__dirname,"..","win_build"));
-
-// console.log(appDir);
+const appDir = path.join(__dirname, "..", "dist", "win-unpacked");
+const outDir = path.join(__dirname, "..", "win_build");
+const gifPath = path.join(__dirname,"load.gid");
 
 resultPromise = electronInstaller.createWindowsInstaller({
     appDirectory: appDir.toString(),
     outputDirectory: outDir.toString(),
     authors: 'My App Inc.',
-    exe: 'myapp.exe'
+    exe: 'myapp.exe',
+    loadingGif : gifPath.toString()
 });
 
 resultPromise.then(() => console.log("It worked!"), (e) => console.log(`No dice: ${e.message}`));
